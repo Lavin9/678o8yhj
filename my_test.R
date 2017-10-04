3467b8086b570368db7e6e4247825c7cc9dcdb36
Lavin9 <bw0123@126.com>
2017-10-04 15:29:53 +0200

    asdfaf

--- my_test.R
+++ my_test.R
@@ -3,5 +3,5 @@ get_mean <- function(x){
   mean <- sum(x)/length(x)
   var <- var(x)
   cat("the  mean of the vector is", mean,"the var is ",var,"\n")
-  return (c(mean,var))}sdfjlkasdfkl;j
-sadfjaldksjfkls;
+  return (c(mean,var))} 
+SSSSSSSSSSSSS
