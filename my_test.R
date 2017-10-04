e4730170a58a76fe0e515ec03efa97ac43165d21
Lavin9 <bw0123@126.com>
2017-10-04 15:21:02 +0200

    hkdh,

--- my_test.R
+++ my_test.R
@@ -1,2 +1,6 @@
-library(dplyr)
-library(ggplot2) #fhfjgkgu
+get_mean <- function(x){
+  x <- x[!is.na(x)] 
+  mean <- sum(x)/length(x)
+  var <- var(x)
+  cat("the  mean of the vector is", mean,"the var is ",var,"\n")
+  return (c(mean,var))}
